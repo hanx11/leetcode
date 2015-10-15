b3241971fdc88bece3510522b2048d757fc78df6
hanx11 <hanx11@163.com>
2015-10-15 23:32:00 +0800

    modify Solution.py

--- Two-Sum/Solution.py
+++ Two-Sum/Solution.py
@@ -2,6 +2,19 @@
 # -*- coding:utf-8 -*-
 
 def twoSum():
-    print("lalala...")
+    numbers = []
+    numbers = eval(input("Please enter an integer arrary: "))
+    target = eval(input("Please enter the target: "))
+    for x in numbers:
+        for y in numbers:
+            if x + y == target:
+                index1 = numbers.index(x)
+                index2 = numbers.index(y)
+                return (index1, index2)
 
-twoSum()
+def main():
+    index1, index2 = twoSum()
+    print("The index1 is", index1+1, ",", "and index2 is", index2+1)
+
+if __name__ == '__main__':
+    main()
